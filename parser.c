94932ae75c84e6aad1318e256653e34e4407d96d
inozo22 <119419194+inozo22@users.noreply.github.com>
2023-12-31 13:00:07 +0100

    works obtain rgb

--- parser.c
+++ parser.c
@@ -6,67 +6,47 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:05:04 by nimai             #+#    #+#             */
-/*   Updated: 2023/12/29 18:02:27 by nimai            ###   ########.fr       */
+/*   Updated: 2023/12/31 12:57:30 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_cub3d.h"
+// #include "ft_cub3d.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include "colors.h"
 
-// #include "libft/libft.h"//for test
+#include "libft/libft.h"//for test
 
-// # define GREEN			"\033[32m"				/* Green */
-// # define RESET			"\033[0m"
+# define GREEN			"\033[32m"				/* Green */
+# define RESET			"\033[0m"
 
 
-// typedef struct	s_point
-// {
-// 	int			x;
-// 	int			y;
-// }				t_point;
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+}				t_point;
 
-// void	flood_fill(char **tab, t_point size, t_point begin);
+void	flood_fill(char **tab, t_point size, t_point begin);
 
 /**
 * @brief obtain data in general (main structure)
+* @param tex_path[0]: noth
+* @param tex_path[1]: south
+* @param tex_path[2]: west
+* @param tex_path[3]: east
 */
-// typedef struct s_data
-// {
-// 	int		num_rows;
-// 	int		num_cols;
-// 	int		num_person;
-// 	t_point	pt_person;
-// }	t_data;
-
-/**
- *	@note 231229 not use
-  */
-  
-// size_t	ft_stroverwrite(char *dst, const char *src, size_t dstsize)
-// {
-// 	size_t	i;
-// 	size_t	srclen;
-
-// 	(void)dstsize;
-// 	printf("overwriting\n");
-// 	printf("%s\n\n", dst);
-// 	// printf("%s\n%s", dst, src);
-	
-// 	srclen = ft_strlen(src);
-// 	i = 0;
-// 	if (dstsize > 0)
-// 	{
-// 		while (src[i] && i < srclen - 1)
-// 		{
-// 			dst[i] = src[i];
-// 			i++;
-// 		}
-// 		// dst[i] = '\0';
-// 	}
-// 	return (srclen);
-// }
+typedef struct s_data
+{
+	int		num_rows;
+	int		num_cols;
+	int		num_person;
+	t_point	pt_person;
+	int		dir_person;
+	char	*tex_path[4];
+	int		floor_col;
+	int		ceiling_col;
+}	t_data;
 
 int	check_file_format(char *str)
 {
@@ -79,6 +59,22 @@ int	check_file_format(char *str)
 		return (-1);
 	}
 	return (0);
+}
+
+int	get_direction(char c)
+{
+	int	ret;
+
+	ret = 0;
+	if (c == 'N')
+		ret = 0;
+	if (c == 'S')
+		ret = 180;
+	if (c == 'W')
+		ret = 270;
+	if (c == 'E')
+		ret = 90;
+	return (ret);
 }
 
 void	count_cols(t_data **data, char *line)
@@ -95,6 +91,7 @@ void	count_cols(t_data **data, char *line)
 			(*data)->num_person++;
 			(*data)->pt_person.x = i;
 			(*data)->pt_person.y = (*data)->num_rows;
+			(*data)->dir_person = get_direction(line[i]);
 		}
 		else if (line[i] && line[i] != '0' && line[i] != '1' && line[i] != 32 && line[i] != 10)
 		{
@@ -109,6 +106,122 @@ void	count_cols(t_data **data, char *line)
 	}
 	if (i > (*data)->num_cols)
 		(*data)->num_cols = i;
+}
+
+/**
+ *@note check before start the main map
+  */
+int	check_paths(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (data->tex_path[i] == NULL)
+		{
+			printf("I don't have enough texture\n");
+			return (0);
+		}
+		i++;
+	}
+	if (!data->ceiling_col || !data->floor_col)
+	{
+		printf("I don't have enough colour\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+  *@param pos[0]: start pos
+  *@param pos[1]: end pos
+  *@param pos[2]: colour position
+  */
+int	get_cols(char *str)
+{
+	int		pos[3];
+	int		colour[3];
+	char	*tmp;
+
+	ft_bzero(pos, sizeof(int) * 3);
+	ft_bzero(colour, sizeof(int) * 3);
+	while (str[pos[0]] && pos[2] < 3)
+	{
+		while (str[pos[0]] && str[pos[0]] == 32)
+			pos[0]++;
+		if (!ft_isdigit(str[pos[0]]))
+			return (printf("colour is not digit"), -1);//error
+		pos[1] = pos[0];
+		while (str[pos[1]] && ft_isdigit(str[pos[1]])/* str[pos[1]] != 32, str[pos[1]] != ',' */)
+			pos[1]++;
+		tmp = ft_substr(str, pos[0], pos[1] - pos[0]);
+		printf("tmp* %s\n", tmp);
+		colour[pos[2]] = ft_atoi(tmp);
+		free (tmp);
+		if (colour[pos[2]] < 0 || colour[pos[2]] > 255)
+			return (printf("colour is out of range"), -1);//error
+		pos[2]++;
+		pos[0] = pos[1] + 1;// I think it's on the comma, so one step ahead
+	}
+	//I need protect from some garbage after colour??
+	if (pos[2] != 3)
+		return (printf("lack of information for colour"), -1);//error OR we can put 0 instead of return error
+	
+	
+	printf("str: %s", str);
+	return (0);
+}
+
+int	obtain_path(t_data **data, char *line)
+{
+	int	i;
+
+	i = 0;
+	if (line[i] && line[i] == 10)
+		return (1);
+	//skip while there are spaces
+	while (line[i] && line[i] == 32)
+		i++;
+	if (&line[i] && !ft_strncmp(&line[i], "NO ", 3))
+	{
+		printf("I got NO\n");
+		return ((*data)->tex_path[0] = ft_strdup(line + (i + 3)), 0);
+	}
+	else if (&line[i] && !ft_strncmp(&line[i], "SO ", 3))
+	{
+		printf("I got SO\n");
+		return ((*data)->tex_path[1] = ft_strdup(line + (i + 3)), 0);
+	}
+	else if (&line[i] && !ft_strncmp(&line[i], "WE ", 3))
+	{
+		printf("I got WE\n");
+		return ((*data)->tex_path[2] = ft_strdup(line + (i + 3)), 0);
+	}
+	else if (&line[i] && !ft_strncmp(&line[i], "EA ", 3))
+	{
+		printf("I got EA\n");
+		return ((*data)->tex_path[3] = ft_strdup(line + (i + 3)), 0);
+	}
+	else if (&line[i] && !ft_strncmp(&line[i], "F ", 2))
+	{
+		printf("I got F\n");
+		return ((*data)->floor_col = get_cols(line + (i + 2)), 0);
+	}
+	else if (&line[i] && !ft_strncmp(&line[i], "C ", 2))
+	{
+		printf("I got C\n");
+		return ((*data)->ceiling_col = get_cols(line + (i + 2)), 0);
+	}
+	// while (line[i])
+	// {
+	// 	if (line[i] && line[i] != 32 && line[i] != '\n')
+	// 		return (-1);
+	// }
+	printf("I got exit\n");
+
+	exit(0);
+	return (0);
 }
 
 void	check_map(t_data **data, char *map_name)
@@ -129,8 +242,16 @@ void	check_map(t_data **data, char *map_name)
 		//also it's neccessary check if there are NO, SU, WE, EA, C, and F before map
 		//
 		printf("%s", line);
-		count_cols(data, line);
-		(*data)->num_rows++;
+		if (check_paths(*data))//if it returns 1
+		{
+			count_cols(data, line);
+			(*data)->num_rows++;
+		}
+		else
+		{
+			if (obtain_path(data, line) == -1)
+				exit (/* close(fd), free (line),  */-4);//error path incorrect
+		}
 		free(line);
 		line = get_next_line(fd);
 	}
@@ -142,7 +263,7 @@ void	replace_spaces(char **str)
 	int	i;
 
 	i = 0;
-	printf("replace: \n%s", *str);
+	// printf("replace: \n%s", *str);
 	while ((*str)[i])
 	{
 		if ((*str)[i] == 32)
@@ -181,6 +302,19 @@ int	is_overflow(char **map, t_data *data)
 	return (1);
 }
 
+void	init_data(t_data **data)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 4)
+	{
+		(*data)->tex_path[i] = NULL;
+	}
+	(*data)->ceiling_col = 0;
+	(*data)->floor_col = 0;
+}
+
 char	**parser(char *map_name, t_data *data)
 {
 	char	**tab;
@@ -189,10 +323,11 @@ char	**parser(char *map_name, t_data *data)
 	int		fd = 0;
 
 	i = -1;
+	init_data(&data);
 	check_map(&data, map_name);
 	//DELETE
-	printf("check! check_map\n");
-	printf("data->row: %d\ndata->cols: %d\ndata->person: %d\n", data->num_rows, data->num_cols, data->num_person);
+	// printf("check! check_map\n");
+	// printf("data->row: %d\ndata->cols: %d\ndata->person: %d\n", data->num_rows, data->num_cols, data->num_person);
 	// data->num_rows = count_rows(map_name);
 	
 	tab = (char **)ft_calloc(data->num_rows + 1, sizeof(char *));
@@ -244,7 +379,7 @@ char	**parser(char *map_name, t_data *data)
 	{
 		tab[i] = ft_strdup(str);
 		if (!tab[i])
-			return (free_2dimension(tab), NULL);
+			return (/* free_2dimension(tab), */ NULL);
 		replace_spaces(&tab[i]);
 		printf("%s", tab[i]);
 		free(str);
@@ -288,7 +423,7 @@ char	**parser(char *map_name, t_data *data)
 	if (is_overflow(tab, data) == -1)
 	{
 		//free memory
-		free_2dimension(tab);
+		// free_2dimension(tab);
 		printf("map is invalid!\n");
 		return (NULL);//error
 	}
@@ -297,29 +432,29 @@ char	**parser(char *map_name, t_data *data)
 
 
 
-// int	main(int ac, char **argv)
-// {
-// 	t_data	data;
-// 	int		fd;
+int	main(int ac, char **argv)
+{
+	t_data	data;
+	int		fd;
 
-// 	ft_bzero(&data, 1 * sizeof(t_data));
-// 	fd = 0;
-// 	if (ac != 2)
-// 	{
-// 		printf("bad argument\n");
-// 		return (-1);//error bad argument
-// 	}
-// 	//check format;
-// 	if (check_file_format(argv[1]) == -1)
-// 		return (-2);//error incorrect file format 
-// 		/* parser */
-// 	char	**map;
-// 	map = parser(argv[1], &data);
-// 	if (!map)
-// 		return(1);//
-// 	printf("output after parser\n");
-// 	for (int i = 0; map[i]; i++)
-// 		printf("%s", map[i]);
-// 	/*  */
-// 	return (0);
-// }
+	ft_bzero(&data, 1 * sizeof(t_data));
+	fd = 0;
+	if (ac != 2)
+	{
+		printf("bad argument\n");
+		return (-1);//error bad argument
+	}
+	//check format;
+	if (check_file_format(argv[1]) == -1)
+		return (-2);//error incorrect file format 
+		/* parser */
+	char	**map;
+	map = parser(argv[1], &data);
+	if (!map)
+		return(1);//
+	printf("output after parser\n");
+	for (int i = 0; map[i]; i++)
+		printf("%s", map[i]);
+	/*  */
+	return (0);
+}
