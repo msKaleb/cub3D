fa662b420745a71de9439a5b5bf39da3dc30ef69
nimai <119419194+inozo22@users.noreply.github.com>
2024-01-03 14:09:57 +0100

    adjust error control

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:05:04 by nimai             #+#    #+#             */
-/*   Updated: 2023/12/31 15:36:34 by nimai            ###   ########.fr       */
+/*   Updated: 2024/01/03 14:07:56 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ int	get_direction(char c)
 	return (ret);
 }
 
-void	count_cols(t_data **data, char *line)
+int	count_cols(t_data **data, char *line)
 {
 	int	i;
 
@@ -98,14 +98,15 @@ void	count_cols(t_data **data, char *line)
 		}
 		else if (line[i] && line[i] != '0' && line[i] != '1' && line[i] != 32 && line[i] != 10)
 		{
-			printf("LINE: %d where am i?? line[i]: %d\n", __LINE__, line[i]);
-			exit(-1);//map invalid (wrong letter)
+			printf("%sMap has invalid letter%s\n", RED, RESET);
+			return(-1);//map invalid (wrong letter)
 		}
 	}
 	if ((*data)->num_person > 1)
-		exit(-1);//map invalid (more than 1 person);
+		return (-1);//map invalid (more than 1 person);
 	if (i > (*data)->map_size.x)
 		(*data)->map_size.x = i;
+	return (0);
 }
 
 /**
@@ -120,83 +121,17 @@ int	check_paths(t_data *data)
 	{
 		if (data->tex_path[i] == NULL)
 		{
-			printf("%sI don't have enough texture%s\n", RED, RESET);
+			printf("%sI don't have enough texture%s\n", GREEN, RESET);
 			return (0);
 		}
 		i++;
 	}
-	if (!data->ceiling_col || !data->floor_col)
+	if (data->ceiling_col == -1 || data->floor_col == -1)
 	{
-		printf("%sI don't have enough colour%s\n", RED, RESET);
+		printf("%sI don't have enough colour%s\n", GREEN, RESET);
 		return (0);
 	}
 	return (1);
-}
-
-/**
-  *@param pos[0]: start pos
-  *@param pos[1]: end pos
-  *@param pos[2]: colour position
-  */
-int	get_cols(char *str)
-{
-	int		pos[3];
-	int		colour[3];
-	char	*tmp;
-	int		ret;
-
-	ft_bzero(pos, sizeof(int) * 3);
-	ft_bzero(colour, sizeof(int) * 3);
-	while (str[pos[0]] && pos[2] < 3)
-	{
-		while (str[pos[0]] && str[pos[0]] == 32)
-			pos[0]++;
-		if (!ft_isdigit(str[pos[0]]))
-			return (printf("colour is not digit"), -1);//error
-		pos[1] = pos[0];
-		while (str[pos[1]] && ft_isdigit(str[pos[1]])/* str[pos[1]] != 32, str[pos[1]] != ',' */)
-			pos[1]++;
-		tmp = ft_substr(str, pos[0], pos[1] - pos[0]);
-		// printf("tmp* %s\n", tmp);
-		colour[pos[2]] = ft_atoi(tmp);
-		free (tmp);
-		if (colour[pos[2]] < 0 || colour[pos[2]] > 255)
-			return (printf("colour is out of range"), -1);//error
-		pos[2]++;
-		pos[0] = pos[1] + 1;// I think it's on the comma, so one step ahead
-	}
-	//I need protect from some garbage after colour??
-	if (pos[2] != 3)
-		return (printf("lack of information for colour"), -1);//error OR we can put 0 instead of return error
-	ret = (colour[0] << 16) + (colour[1] << 8) + (colour[2]);
-	printf("ret: %d\n", ret);
-	return (ret);
-}
-
-int	obtain_path(t_data **data, char *line)
-{
-	int	i;
-
-	i = 0;
-	//move to the next line if there is only '\n'
-	if (line[i] && line[i] == 10)
-		return (1);
-	//skip while there are spaces
-	while (line[i] && line[i] == 32)
-		i++;
-	if (&line[i] && !ft_strncmp(&line[i], "NO ", 3))
-		return ((*data)->tex_path[0] = ft_strdup(line + (i + 3)), 0);
-	else if (&line[i] && !ft_strncmp(&line[i], "SO ", 3))
-		return ((*data)->tex_path[1] = ft_strdup(line + (i + 3)), 0);
-	else if (&line[i] && !ft_strncmp(&line[i], "WE ", 3))
-		return ((*data)->tex_path[2] = ft_strdup(line + (i + 3)), 0);
-	else if (&line[i] && !ft_strncmp(&line[i], "EA ", 3))
-		return ((*data)->tex_path[3] = ft_strdup(line + (i + 3)), 0);
-	else if (&line[i] && !ft_strncmp(&line[i], "F ", 2))
-		return ((*data)->floor_col = get_cols(line + (i + 2)), 0);
-	else if (&line[i] && !ft_strncmp(&line[i], "C ", 2))
-		return ((*data)->ceiling_col = get_cols(line + (i + 2)), 0);
-	return (-1);
 }
 
 int	is_brank(char *line)
@@ -211,6 +146,77 @@ int	is_brank(char *line)
 		i++;
 	}
 	return (1);
+}
+
+/**
+  *@param pos[0]: start pos
+  *@param pos[1]: end pos
+  *@param pos[2]: colour position
+  *@note convert rgb to colour in dicimal
+  */
+int	get_rgb(char *str)
+{
+	int		pos[3];
+	int		colour[3];
+	char	*tmp;
+	int		ret;
+
+	ft_bzero(pos, sizeof(int) * 3);
+	ft_bzero(colour, sizeof(int) * 3);
+	while (str[pos[0]] && pos[2] < 3)
+	{
+		while (str[pos[0]] && str[pos[0]] == 32)
+			pos[0]++;
+		if (!ft_isdigit(str[pos[0]]))
+			return (printf("colour is not digit\n"), -1);//error
+		pos[1] = pos[0];
+		while (str[pos[1]] && ft_isdigit(str[pos[1]])/* str[pos[1]] != 32, str[pos[1]] != ',' */)
+			pos[1]++;
+		tmp = ft_substr(str, pos[0], pos[1] - pos[0]);
+		// printf("tmp* %s\n", tmp);
+		colour[pos[2]] = ft_atoi(tmp);
+		free (tmp);
+		if (colour[pos[2]] < 0 || colour[pos[2]] > 255)
+			return (printf("colour is out of range\n"), -1);//error
+		pos[2]++;
+		pos[0] = pos[1] + 1;
+	}
+	//I need protect from some garbage after colour??
+	if (pos[2] != 3)
+		return (printf("lack of information for colour\n"), -1);//error OR we can put 0 instead of return error
+	if (!is_brank(&str[pos[0]]))
+		return (printf("I have more than I should have\n"), -1);//error OR we can put 0 instead of return error
+	ret = (colour[0] << 16) + (colour[1] << 8) + (colour[2]);
+	printf("ret: %d\n", ret);
+	return (ret);
+}
+
+int	obtain_path(t_data **data, char *line)
+{
+	int	i;
+	int	colour;
+
+	i = 0;
+	colour = 0;
+	//move to the next line if there is only '\n'
+	if (!line || (line[i] && line[i] == 10))
+		return (1);
+	//skip while there are spaces
+	while (line[i] && line[i] == 32)
+		i++;
+	if (!ft_strncmp(&line[i], "NO ", 3))
+		return ((*data)->tex_path[0] = ft_strdup(line + (i + 3)), 0);
+	else if (!ft_strncmp(&line[i], "SO ", 3))
+		return ((*data)->tex_path[1] = ft_strdup(line + (i + 3)), 0);
+	else if (!ft_strncmp(&line[i], "WE ", 3))
+		return ((*data)->tex_path[2] = ft_strdup(line + (i + 3)), 0);
+	else if (!ft_strncmp(&line[i], "EA ", 3))
+		return ((*data)->tex_path[3] = ft_strdup(line + (i + 3)), 0);
+	else if (!ft_strncmp(&line[i], "F ", 2))
+		return ((*data)->floor_col = get_rgb(line + (i + 2)));
+	else if (!ft_strncmp(&line[i], "C ", 2))
+		return ((*data)->ceiling_col = get_rgb(line + (i + 2)));
+	return (-1);
 }
 
 int	check_map(t_data **data, char *map_name)
@@ -231,7 +237,8 @@ int	check_map(t_data **data, char *map_name)
 		{
 			if (!(*data)->pos_map)
 				(*data)->pos_map = i;
-			count_cols(data, line);
+			if (count_cols(data, line) == -1)
+				return (close(fd), free (line), -1);
 			(*data)->map_size.y++;
 		}
 		else
@@ -254,7 +261,7 @@ void	replace_spaces(char **str)
 	{
 		if ((*str)[i] == 32)
 			(*str)[i] = '0';
-		if ((*str)[i] == 'N' || (*str)[i] == 'S' || (*str)[i] == 'W' || (*str)[i] == 'E')
+		if ((*str)[i] >= 'E' && (*str)[i] <= 'W')
 			(*str)[i] = '0';
 		i++;
 	}
@@ -265,22 +272,19 @@ int	is_overflow(char **map, t_data *data)
 	int	i;
 
 	i = 0;
-	//the first line, the last line check
-	
-	//skip until the wall
 	while (map[data->map_size.y - 1 - i][0] == '\n')
 		i++;
 	if (ft_strchr(map[0], 'F') || ft_strchr(map[data->map_size.y - 1 - i], 'F'))
 	{
-		printf("the first\n");
+		printf("the first or the last line\n");
 		return (-1);
 	}
 	i = 0;
-	//check each line the first letter and the last(without '\n')
 	while (map[i] && i < data->map_size.y)
 	{
 		if (map[i][0] == 'F' || map[i][(int)ft_strlen(map[i]) - 2] == 'F')
 		{
+			printf("the right or the left edge\n");
 			return (-1);
 		}
 		i++;
@@ -297,12 +301,8 @@ void	init_data(t_data **data)
 	{
 		(*data)->tex_path[i] = NULL;
 	}
-	(*data)->ceiling_col = 0;
-	(*data)->floor_col = 0;
-	/**
-	* @note CHECK! it's initialized as 0 at home, how about MAC?
-	  */
-	printf("pos_map: %d\n", (*data)->pos_map);
+	(*data)->ceiling_col = -1;
+	(*data)->floor_col = -1;
 }
 
 char	**obtain_map(t_data **data, int fd)
@@ -343,7 +343,7 @@ char	**parser(char *map_name, t_data *data)
 /**
  * @note check! if the initialization works well. If so, remove init_data
   */
-	// init_data(&data);
+	init_data(&data);
 	fd = 0;
 	if (check_map(&data, map_name) == -1)
 		return (NULL);
@@ -355,7 +355,7 @@ char	**parser(char *map_name, t_data *data)
 		return (NULL);//error obtain_map failed
 	flood_fill(tab, data->map_size, data->pt_person);
 	if (is_overflow(tab, data) == -1)
-		return (printf("map is invalid!\n"),/* free_2dimension(tab) */NULL);//error
+		return (printf("%smap is not closed%s\n", RED, RESET),/* free_2dimension(tab) */NULL);//error
 	return (tab);
 }
 
@@ -381,6 +381,7 @@ int	main(int ac, char **argv)
 	map = parser(argv[1], &data);
 	if (!map)
 	{
+		//free
 		printf("failed parser\n");
 		return(1);//
 	}
