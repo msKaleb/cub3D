197e391f53e1c79eb9cda04bb446cbbb3f401e8c
msKaleb <sjmikel@gmx.es>
2024-01-14 10:47:37 +0100

    leak hunting

--- mlx_utils.c
+++ mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: msoria-j <msoria-j@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:37:30 by msoria-j          #+#    #+#             */
-/*   Updated: 2024/01/13 23:29:48 by msoria-j         ###   ########.fr       */
+/*   Updated: 2024/01/14 10:45:46 by msoria-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,24 @@
 
 /**
  * @brief free memory and destroy the mlx window
+ * @note using MAX_TEXTURES for weapon free because uses the same amount
+ * of images as textures array
+ * @todo free minimap, but in mandatory there is no minimap. Avoid duplicating close_mlx
   */
 int	close_mlx(t_mlx *m)
 {
-	free_map(&m->player);
+	int	i;
+
+	i = -1;
+	while (++i < MAX_TEXTURES)
+	{
+		if (m->player.text[i].texture)
+			mlx_destroy_image(m->mlx, m->player.text[i].texture);
+		if (m->player.weapon[i].img)
+			mlx_destroy_image(m->mlx, m->player.weapon[i].img);
+	}
+	free_2dimension(m->player.map);
+	free_data(m->player.data);
 	if (m->mlx && m->img)
 		mlx_destroy_image(m->mlx, m->img);
 	if (m->win)
@@ -32,17 +46,23 @@ int	close_mlx(t_mlx *m)
   */
 void	init_mlx(t_mlx *m)
 {
+	int	i;
+	
+	i = -1;
+	while (++i < MAX_TEXTURES)
+	{
+		m->player.text[i].texture = NULL;
+		m->player.weapon[i].img = NULL;
+	}
 	m->mlx = NULL;
 	m->win = NULL;
 	m->img = NULL;
 	m->mlx = mlx_init();
-	if (m->mlx == NULL)
-		exit (err_mlx(m));
-	m->win = mlx_new_window(m->mlx, DEFAULT_X, DEFAULT_Y, \
+	if (m->mlx != NULL)
+		m->win = mlx_new_window(m->mlx, DEFAULT_X, DEFAULT_Y, \
 		"cub3D Project - nimai & msoria-j");
-	if (m->win == NULL)
-		exit(err_mlx(m));
-	m->img = mlx_new_image(m->mlx, DEFAULT_X, DEFAULT_Y);
+	if (m->win != NULL)
+		m->img = mlx_new_image(m->mlx, DEFAULT_X, DEFAULT_Y);
 	if (m->img == NULL)
 		exit(err_mlx(m));
 	m->addr = mlx_get_data_addr(m->img, &m->bpp, &m->sl, &m->endian);
